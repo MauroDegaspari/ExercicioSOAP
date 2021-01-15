9c28dd87e5ea71350489b3109fb41f9aa5cc165d
MauroDegaspari <mauro_degaspari@hotmail.com>
2021-01-15 15:59:28 -0300

    Atualização AgendaIm

---
+++ src/repositorio/Repositorio.java
@@ -0,0 +1,12 @@
+package repositorio;
+
+import java.util.List;
+
+import entidade.Cliente;
+
+public class Repositorio {
+
+	public List<Cliente> PesquisarCliente(Cliente cliente){
+		return null;
+	}
+}
